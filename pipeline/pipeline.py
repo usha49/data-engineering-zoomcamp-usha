f30140ca00c9de82835874dbfb0000e81b935321
usha49 <122425849+usha49@users.noreply.github.com>
2026-01-16 10:54:49 +0000

    Pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+print("arguments", sys.argv)
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
